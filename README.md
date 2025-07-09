67adbed166d8301db1e438c1be1a825c887af700
WebberLiao <WebberLiao@users.noreply.github.com>
2025-07-09 05:32:17 +0000

    deploy: a796b0728bd62354cb26005691b27efb006ec78e

---
+++ README.md
@@ -0,0 +1,33 @@
+<link rel="stylesheet" type="text/css" href="styles.css">
+
+# Webber Liao's Notes
+
+## Linux
+### [OpenWrt](/Linux/OpenWrt/OpenWrt.html)
+### [UBUS](/Linux/OpenWrt/ubus.html)
+### [UCI](/Linux/OpenWrt/uci.html)
+### [Commands](/Linux/Commands.html)
+### [Other](/Linux/Other.html)
+
+## Networking
+### [L2 Protocols](/Networking/L2_protocols/L2_protocols.html)
+### [L3 Protocols](/Networking/L3_protocols/L3_protocols.html)
+### [Other Protocols](/Networking/Others/Others.html)
+### [Wi-Fi](/Networking/WiFi/Wi-Fi.html)
+
+## Git
+### [Git](/Other/Git/Git.html)
+### [GitHub](/Other/Git/GitHub.html)
+
+## Other
+### [Docker](/Other/Docker/Docker.html)
+### [Dockerfile](/Other/Docker/Dockerfile.html)
+### [MQTT mosquitto](/Other/MQTT/mosquitto.html)
+
+## Programming
+### [C](/Programming/C.html)
+### [Markdown](/Programming/Markdown.html)
+### [Rust](/Programming/Rust.html)
+
+## Contact Me
+### [Contact Me](mailto:webberliao0776@gmail.com?subject=To%20Webber&body=Hi%20Webber)
---
+++ build.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+// const markdown = require('markdown-to-html').Markdown;
+const marked = require('marked');
+const inputDir = './'; // Change this to your input directory if needed
+const outputDir = './'; // Change this to your output directory if needed
+
+// Function to convert a single Markdown file to HTML
+const convertMarkdownToHtml = (inputFile, outputFile) => {
+    fs.readFile(inputFile, 'utf8', (err, data) => {
+        if (err) {
+            console.error(`Error reading file ${inputFile}: ${err}`);
+            return;
+        }
+
+        // Convert Markdown to HTML
+        // markdown({ gfm: true })(data, (err, html) => {
+        //     if (err) {
+        //         console.error(`Error converting ${inputFile} to HTML: ${err}`);
+        //         return;
+        //     }
+            const html = marked(data);
+            // Write the HTML to a file
+            fs.writeFile(outputFile, html, (err) => {
+                if (err) {
+                    console.error(`Error writing HTML file ${outputFile}: ${err}`);
+                    return;
+                }
+                console.log(`Successfully converted ${inputFile} to ${outputFile}`);
+            });
+        // });
+    });
+};
+
+// Read all .md files in the input directory
+fs.readdir(inputDir, (err, files) => {
+    if (err) {
+        console.error(`Error reading directory ${inputDir}: ${err}`);
+        return;
+    }
+
+    files.forEach(file => {
+        if (path.extname(file) === '.md') {
+            const inputFile = path.join(inputDir, file);
+            const outputFile = path.join(outputDir, file.replace(/\.md$/, '.html'));
+            convertMarkdownToHtml(inputFile, outputFile);
+        }
+    });
+});
